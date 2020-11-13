f221634a6c86609c4d9002b8e4ddb5c3b2600ada
Ustariz Enzo <enzo.ustariz@hotmail.fr>
2020-11-13 19:49:26 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,8 +2,18 @@ import axios from 'axios';
 
 let api = axios.create({
     headers: {
-        'Client-ID' : 'ogyae7e2msxh6tnjakbl5s3adq3tnq'
-    }
+        "Client-ID": "VOTRE_CLIENT_ID",
+        "Authorization": "Bearer VOTRE_JETON"
+        }
 })
+
+/*
+    CLIENT_ID = 'VOTRE_CLIENT_ID';
+    REDIRECT = 'http://127.0.0.1/';
+
+    LIEN AUTH = https://id.twitch.tv/oauth2/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT}&response_type=token
+
+
+*/
 
 export default api;
